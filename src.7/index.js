9eff7254ec8b080f58aab779ebaeec0ad02999a8
Pedro Fausto <pedro.fausto@hotmail.com>
2018-10-15 18:06:34 -0300

    redux

--- src.7/index.js
+++ src.7/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { combineReducers, createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-import fieldReducer from './componet/fieldReducer'
 import Field from './componet/field';
 import * as serviceWorker from './serviceWorker';
 
-const reducers = combineReducers({
-  field: fieldReducer
-})
-
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <Field initialValue='Teste' />
-  </Provider>
+  <Field initialValue='Teste' />
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
